944075aeb1b6daffe38cf7e33baa172b3bf34bdd
jonaditommaso <jonaditommaso@gmail.com>
2024-03-12 00:49:33 -0300

    chore: remove out folder

--- out/utils/functionsInfo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.functionInfos = void 0;
-exports.functionInfos = [
-    { name: 'useEffect', message: 'UseEffect hooks:' },
-    { name: 'useState', message: 'UseState hooks:' },
-    { name: 'functionalComponent', message: 'Functional components:' },
-];
-//# sourceMappingURL=functionsInfo.js.map
--- out/utils/regex.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REGEX = void 0;
-exports.REGEX = {
-    useEffect: /(?<!\bimport\s.*{[^}]*\b)useEffect\b(?![^}]*\b\})/g,
-    useState: /(?<!\bimport\s.*{[^}]*\b)(useState|React\.useState)\b(?![^}]*\b\})/g,
-    functionalComponent: /\b(?:function|const)\s+([A-Z][a-zA-Z\d]*)\s*\(.*\)|export\s+(?:const|function)\s+([A-Z][a-zA-Z\d]*)|\bexport\s+default\s+([A-Z][a-zA-Z\d]*)\b/g
-};
-//# sourceMappingURL=regex.js.map
